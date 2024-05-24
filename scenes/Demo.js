d09893c61f68f150a911b22bbc85063feafb9f4a
Kirk Morris <worker.kirk@gmail.com>
2024-05-24 12:55:08 -0700

    Dev mode.

--- scenes/Demo.js
+++ scenes/Demo.js
@@ -182,7 +182,7 @@ export class Demo extends Scene {
     this.villain = new Villain(this, 0, 0);
     this.villain.setVisible(false);
 
-    this.devMode = true && (process.env.NODE_ENV === 'development');
+    this.devMode = (process.env.NODE_ENV === 'development');
 
     this.cameras.main.setZoom(3);
     // this.cameras.main.setZoom(1);
